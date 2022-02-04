bdd69f2d46ca95a555110043416a3a7ded06ae11
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-02-05 03:03:48 +0600

    https://leetcode.com/problems/remove-nth-node-from-end-of-list/ - V3

--- src/main/java/com/studevs/experiment/myleetcodesolutions/solution/medium/RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/studevs/experiment/myleetcodesolutions/solution/medium/RemoveNthNodeFromEndOfList.java
@@ -1,7 +1,6 @@
 package com.studevs.experiment.myleetcodesolutions.solution.medium;
 
 import java.io.Serializable;
-import java.util.Stack;
 
 /**
  * URL: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
@@ -26,18 +25,29 @@ public class RemoveNthNodeFromEndOfList implements Serializable {
 	}
 	
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		Stack<ListNode> stack = new Stack<>();
-		while (head != null) {
-			stack.add(head);
-			head = head.next;
-		}
-		int count = 0;
-		while (!stack.empty()) {
-			ListNode val = stack.pop();
-			if (++count != n) {
-				val.next = head;
-				head = val;
+		return this.revertAndRemove(this.revert(head), n);
+	}
+	
+	private ListNode revertAndRemove(ListNode current, int n) {
+		ListNode head = null;
+		while (current != null) {
+			ListNode temp = current;
+			current = current.next;
+			if (--n != 0) {
+				temp.next = head;
+				head = temp;
 			}
+		}
+		return head;
+	}
+	
+	private ListNode revert(ListNode current) {
+		ListNode head = null;
+		while (current != null) {
+			ListNode temp = current;
+			current = current.next;
+			temp.next = head;
+			head = temp;
 		}
 		return head;
 	}
